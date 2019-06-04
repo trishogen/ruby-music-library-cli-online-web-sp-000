c27b3bff65202db8c4c692080cab30b1a26bffc4
Trish Ogen <patriciaogen@gmail.com>
2019-06-04 22:54:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,12 +49,7 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    artist_name = filename.split(/ - |.mp3/)[0]
-    song_name = filename.split(/ - |.mp3/)[1]
-    genre_name = filename.split(/ - |.mp3/)[2]
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
+    self.new_from_filename(filename)
   end
 
 end
