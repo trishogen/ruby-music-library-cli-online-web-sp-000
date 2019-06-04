98711e5000adfa098f4d0a3c9d5d5e6e3324e3b9
Trish Ogen <patriciaogen@gmail.com>
2019-06-04 23:28:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,8 +26,8 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort_by {|song| song.name}
-    Song.all.each_with_index do |song, index|
+    songs_sorted = Song.all.sort_by {|song| song.name}
+    songs_sorted.each_with_index do |song, index|
       puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
