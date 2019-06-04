1b4e8061b18d8d21a6b94cda198feeec0047de75
Trish Ogen <patriciaogen@gmail.com>
2019-06-04 23:09:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,9 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets
+    if input != 'quit'
+      input = gets
+    end
   end
 
 end
