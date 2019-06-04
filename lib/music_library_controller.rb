5c151c001a817c3cf17621fdea292556a6953871
Trish Ogen <patriciaogen@gmail.com>
2019-06-04 23:01:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,9 +1,10 @@
-class MusicLibraryController
+class MusicLibraryController << MusicImporter
 
   attr_reader :path
 
   def initialize(path)
     @path = path
+    MusicImporter.new(path)
   end
   
 end
