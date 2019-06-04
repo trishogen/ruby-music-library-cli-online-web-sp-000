022eba7dfe074838ee32222a278e0f167e4a9cff
Trish Ogen <patriciaogen@gmail.com>
2019-06-04 23:41:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -49,7 +49,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist_input = gets.chomp
-    Artist.find_by_name(artist_input).songs.each_with_index do |song, index|
+    Artist.find_or_create_by_name(artist_input).songs.each_with_index do |song, index|
       puts "#{index + 1}. #{song.name}"
     end
   end
