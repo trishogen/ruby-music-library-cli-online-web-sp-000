d0becdeb2629d7a1f0dee0ad7106a44a4e2fd12b
Trish Ogen <patriciaogen@gmail.com>
2019-06-04 23:07:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,6 +16,7 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
+    puts "What would you like to do?"
   end
 
 end
