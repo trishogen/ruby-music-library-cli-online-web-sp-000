f3b47e2278ccfd7f2ae31094cef0ccf7945491ce
Trish Ogen <patriciaogen@gmail.com>
2019-06-05 00:10:06 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,8 +71,8 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     song_index = gets.chomp.to_i - 1
     songs = Song.all.sort_by {|song| song.name}
-    song = Song.all.sort_by {|song| song.name}[song_index]
     if (0...(songs.size + 1)).include? song_index
+      song = Song.all.sort_by {|song| song.name}[song_index]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
