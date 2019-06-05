4d145f12486fdc5d90dddebaf2fe238dfe6866be
Trish Ogen <patriciaogen@gmail.com>
2019-06-05 00:19:22 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -31,6 +31,8 @@ class MusicLibraryController
         list_songs_by_artist
       elsif input == 'list genre'
         list_songs_by_genre
+      elsif input == 'play_song'
+        play_song
       end
       input = gets.chomp
     end
